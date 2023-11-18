428123b50711ddc33fecf9e993ac5ac4da6b1542
arundalakoti <arun.dalakoti@lenskart.in>
2023-11-18 19:03:47 +0530

    added react-hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import './App.css'
 
+import { useEffect } from 'react'
+import toast from 'react-hot-toast'
+
 function App() {
+
+  useEffect(() => {
+    toast.success("success");
+  },[])
+
   return (
     <>
       hello
